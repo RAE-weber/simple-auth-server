53e87af140e754fdaca488e7effd01c198a5137a
rae <156593905+RAE-weber@users.noreply.github.com>
2026-04-14 22:35:51 +0800

    feat: add '/login' route with validation

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,41 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
+import * as z from "zod";
 
-const app = new Hono()
+const loginSchema = z.object({
+	email: z.email(),
+	password: z.string().min(8).max(16),
+});
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+type LoginRequest = z.infer<typeof loginSchema>;
+
+const app = new Hono();
+
+app.get("/", (c) => {
+	return c.text("Hello Hono!");
+});
+
+app.post("/login", async (c) => {
+	try {
+		const payload = await c.req.json();
+		const validated = loginSchema.safeParse(payload);
+
+		if (!validated.success) {
+			return c.json({ error: "Invalid Request Body" }, 400);
+		}
+
+		const { email, password } = validated.data as LoginRequest;
+
+		return c.json({
+			message: "Login Successful",
+			email: email,
+			password: password,
+		});
+	} catch (error) {
+		return c.json({ error: "Invalid Request Body" }, 400);
+	}
+});
 
 export default {
-  port : 80,
-  fetch : app.fetch,
-}
+	port: 80,
+	fetch: app.fetch,
+};
